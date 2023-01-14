9d0e0732e8dfa40f91ed6396b7af552558181cc7
mldamico <matias.lioneldamico@gmail.com>
2023-01-14 16:58:24 -0300

    Basic layout

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Ticket from "../images/movie-tickets.webp";
+export const Home = () => {
+  return (
+    <>
+      <div className="text-center">
+        <h2>Find a movie to watch tonight!</h2>
+        <hr />
+        <img src={Ticket} alt="Movie Tickets" style={{ width: 200 }} />
+      </div>
+    </>
+  );
+};
