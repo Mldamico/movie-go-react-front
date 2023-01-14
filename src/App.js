a87674839369b04ed2a5d7cd473966921570f2fa
mldamico <matias.lioneldamico@gmail.com>
2023-01-14 16:45:46 -0300

    movie-front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1 className="mt-3">Go Watch a Movie!</h1>
+        </div>
+        <div className="col text-end">
+          <a href="#">
+            <span className="badge badge-success">Login</span>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
