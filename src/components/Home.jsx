8bf23c2b4481a369bf6ac86a0693e9be2fb99e54
mldamico <matias.lioneldamico@gmail.com>
2023-01-14 18:26:14 -0300

    Fake movies data and showing info

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Ticket from "../images/movie-tickets.webp";
 export const Home = () => {
   return (
@@ -6,7 +7,9 @@ export const Home = () => {
       <div className="text-center">
         <h2>Find a movie to watch tonight!</h2>
         <hr />
-        <img src={Ticket} alt="Movie Tickets" style={{ width: 200 }} />
+        <Link to="/movies">
+          <img src={Ticket} alt="Movie Tickets" style={{ width: 200 }} />
+        </Link>
       </div>
     </>
   );
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,10 +1,52 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+const moviesList = [
+  {
+    id: 1,
+    title: "Highlander",
+    release_date: "1986-03-07",
+    runtime: 116,
+    mpaa_rating: "R",
+    description:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. At, voluptatem. A, pariatur obcaecati reprehenderit omnis dolore autem dolor et eius nisi possimus corrupti nihil. Recusandae ad nemo sint quis eaque.",
+  },
+  {
+    id: 2,
+    title: "Raiders of the lost ark",
+    release_date: "1986-06-12",
+    runtime: 115,
+    mpaa_rating: "PG-13",
+    description:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. At, voluptatem. A, pariatur obcaecati reprehenderit omnis dolore autem dolor et eius nisi possimus corrupti nihil. Recusandae ad nemo sint quis eaque.",
+  },
+];
 export const Movies = () => {
+  const [movies, setMovies] = useState(moviesList);
+
   return (
-    <>
-      <div className="text-center">
-        <h2>Movies</h2>
-        <hr />
-      </div>
-    </>
+    <div className="text-center">
+      <h2>Movies</h2>
+      <hr />
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Movie</th>
+            <th>Release Date</th>
+            <th>Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          {movies.map((movie) => (
+            <tr key={movie.id}>
+              <td>
+                <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+              </td>
+              <td>{movie.release_date}</td>
+              <td>{movie.mpaa_rating}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
