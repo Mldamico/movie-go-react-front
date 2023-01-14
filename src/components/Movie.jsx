7c5f63caf51933d68cdbbfb40ba82e2818a9b37f
mldamico <matias.lioneldamico@gmail.com>
2023-01-14 18:52:38 -0300

    Fake Data

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { moviesList } from "./Movies";
+
 export const Movie = () => {
+  const [movie, setMovie] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    let foundMovie = moviesList.find((m) => m.id === +id);
+
+    if (foundMovie) {
+      setMovie(foundMovie);
+    }
+  }, [id, movie]);
+
   return (
-    <>
-      <div className="text-center">
-        <h2>Movie</h2>
-        <hr />
-      </div>
-    </>
+    <div className="text-center">
+      <h2>{movie.title}</h2>
+      <small>
+        <em>
+          {movie.release_date}, {movie.runtime} minutes, Rated{" "}
+          {movie.mpaa_rating}
+        </em>
+      </small>
+      <hr />
+      {movie.description}
+    </div>
   );
 };
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-const moviesList = [
+export const moviesList = [
   {
     id: 1,
     title: "Highlander",
