46da6a7e6f6a703eb5bb88db64a6b7a270833065
mldamico <matias.lioneldamico@gmail.com>
2023-01-14 20:13:25 -0300

    Form in login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,39 @@
+import { useState } from "react";
+import { Input } from "./form/Input";
+
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (email === "admin@example.com" && password === "123456") {
+    }
+  };
   return (
-    <>
-      <div className="text-center">
-        <h2>Login</h2>
+    <div className="col-md-6 offset-md-3">
+      <h2>Login</h2>
+      <hr />
+      <form onSubmit={handleSubmit}>
+        <Input
+          title="Email Address"
+          type="email"
+          className="form-control"
+          name="email"
+          autoComplete="email-new"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          title="Password"
+          type="password"
+          className="form-control"
+          name="password"
+          autoComplete="password-new"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <hr />
-      </div>
-    </>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+    </div>
   );
 };
