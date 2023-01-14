0f6a437c9c05b4b89964081b360def4df72937b6
mldamico <matias.lioneldamico@gmail.com>
2023-01-14 19:56:36 -0300

    Conditional show with jwttoken

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 function App() {
+  const [jwtToken, setJwtToken] = useState("");
   return (
     <div className="container">
       <div className="row">
@@ -8,9 +10,15 @@ function App() {
           <h1 className="mt-3">Go Watch a Movie!</h1>
         </div>
         <div className="col text-end">
-          <Link to="/login">
-            <span className="badge bg-success">Login</span>
-          </Link>
+          {jwtToken === "" ? (
+            <Link to="/login">
+              <span className="badge bg-success">Login</span>
+            </Link>
+          ) : (
+            <Link to="#!">
+              <span className="badge bg-danger">Logout</span>
+            </Link>
+          )}
         </div>
         <hr className="mb-3" />
       </div>
@@ -33,24 +41,28 @@ function App() {
               >
                 Genres
               </a>
-              <Link
-                to="/admin/movie/0"
-                className="list-group-item list-group-item-action"
-              >
-                Add Movie
-              </Link>
-              <Link
-                to="/manage"
-                className="list-group-item list-group-item-action"
-              >
-                Manage
-              </Link>
-              <Link
-                to="/graphql"
-                className="list-group-item list-group-item-action"
-              >
-                GraphQL
-              </Link>
+              {jwtToken !== "" && (
+                <>
+                  <Link
+                    to="/admin/movie/0"
+                    className="list-group-item list-group-item-action"
+                  >
+                    Add Movie
+                  </Link>
+                  <Link
+                    to="/manage"
+                    className="list-group-item list-group-item-action"
+                  >
+                    Manage
+                  </Link>
+                  <Link
+                    to="/graphql"
+                    className="list-group-item list-group-item-action"
+                  >
+                    GraphQL
+                  </Link>
+                </>
+              )}
             </div>
           </nav>
         </div>
